4334b2c3a0c8270d162f3d203a4b0679061226fb
Jesse <jessegreen0906@gmail.com>
2018-12-31 15:43:18 +1100

    Interim commit. Trying to get storybook working.

---
+++ client/src/.storybook/config.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018. Code contained in this file is written by and the intellectual property of Jesse Green except where otherwise noted. Reuse for educational, open source or reference uses is freely available provided reference to the original source is cited with use. Reuse for commercial purposes is prohibited without the consent of the owner.
+ */
+
+import { configure } from "@storybook/react";
+
+function loadStories() {
+	
+}
+
+configure(loadStories, module);
